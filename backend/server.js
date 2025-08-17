d6824e0768e9b1a24ba206cc0b37454e075d6078
hitesh-ydv <hiteshyadav124001@gmail.com>
2025-08-17 19:24:44 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Syllabus API is running...");
+  res.send("API is running...");
 });
 
 app.use("/api/syllabus", syllabusRoutes);
